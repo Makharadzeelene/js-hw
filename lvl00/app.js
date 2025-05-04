7fe7ca8e7199a0f749e58e7a49b8f413a81eb175
Makharadzeelene <146019882+Makharadzeelene@users.noreply.github.com>
2025-05-04 20:49:38 +0400

    Add files via upload

---
+++ lvl00/app.js
@@ -0,0 +1,39 @@
+// let nickName = "miko"
+
+// let age = 14
+
+// let isOnline = true
+
+// console.log(typeof(nickName))
+// console.log(typeof(age))
+// console.log(typeof(isOnline))
+
+// hw#2
+
+// let name = "elene";
+
+// let lastName = "makharadze";
+
+// let year = 2013;
+
+// let age = 12;
+
+// let livesIn = "dighomi";
+
+// let activeRn = true;
+
+// console.log(`hello my names ${name}, my lastnames ${lastName}, i was born in ${year} so that makes me ${age} yo rn. i live in ${livesIn}.`);
+// console.log(`elene is active rn - ${activeRn}`);
+
+// hw#3
+
+//let num = 11
+
+//num = '11'
+
+//console.log(typeof(num))
+//let number = "11"
+
+//number = 11
+
+//console.log(typeof(number))
