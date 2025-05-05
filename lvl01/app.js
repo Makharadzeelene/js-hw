48f02d086a107cc66251b8e5e73de14882a6adc1
Makharadzeelene <146019882+Makharadzeelene@users.noreply.github.com>
2025-05-05 16:33:05 +0400

    Add files via upload

---
+++ lvl01/app.js
@@ -0,0 +1,20 @@
+// hw#1
+// let a = 10 + 12 - 2
+// let b = 92
+
+// let extraAlphabet = a + b
+
+// console.log(extraAlphabet)
+
+// let c = 12 - 6 + 5;
+// let d = 21;
+
+// let e = 3 +  3 + 3;
+// let f = 9;
+
+// let g = 1 + 0 * 13;
+// let h = 10;
+
+// let i = 23 + 2  * 7;
+// let j = 13;
+
