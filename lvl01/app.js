73cbe2c603752be0fe4837d2020e23ce6be8f3f5
Makharadzeelene <146019882+Makharadzeelene@users.noreply.github.com>
2025-05-06 21:50:36 +0400

    Add files via upload

--- lvl01/app.js
+++ lvl01/app.js
@@ -18,3 +18,55 @@
 // let i = 23 + 2  * 7;
 // let j = 13;
 
+
+
+// hw#2
+// let l = 3;
+// const m = l--;
+
+// console.log(`${l} ${m}`)
+
+// let n = 5;
+// const o = ++n;
+
+// console.log(`${n} ${o}`)
+
+// let p = 7;
+// const q = p++;
+
+// console.log(`${p} ${q}`)
+
+// let r = 12;
+// const s = ++r;
+
+// console.log(`${r} ${s}`)
+
+// let t = 14;
+// const u = t--;
+
+// console.log(`${t} ${u}`)
+
+// let  v = 16;
+// const w = v++;
+
+// console.log(`${v} ${w}`)
+
+// let x = 18;
+// const y = --x;
+
+// console.log(`${x} ${y}`)
+
+// let z = 11;
+// const A = z++;
+
+// console.log(`${z} ${A}`)
+
+// let B = 9;
+// const C = ++B;
+
+// console.log(`${B} ${C}`)
+
+// let D = 8;
+// const E = D--;
+
+// console.log(`${D} ${E}`)
