eccdce4b334b63a4691a9418d932e9df195de43e
Makharadzeelene <146019882+Makharadzeelene@users.noreply.github.com>
2025-05-09 14:19:45 +0400

    Add files via upload

---
+++ lvl05/app.js
@@ -0,0 +1,20 @@
+// hw#1
+// let students = 19
+
+// if(students >= 25){
+//     console.log('youre correct i guess ur smarter then me:O')
+// } 
+// else{
+//     console.log('oh so u aint so smart..')
+// }
+
+// hw#2
+// function calculateRectangleArea(length, height) {
+//     return length * height;
+//   }
+  
+//   // მაგალითი გამოყენების:
+//   const area = calculateRectangleArea(5, 3);
+//   console.log("მართკუთხედის ფართობი არის:", area);
+
+// hw#3
