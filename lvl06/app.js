ec4534f9cd55eec0b8adc62bdfdb0921596a3dab
Makharadzeelene <146019882+Makharadzeelene@users.noreply.github.com>
2025-05-13 22:36:12 +0400

    Add files via upload

---
+++ lvl06/app.js
@@ -0,0 +1,202 @@
+// hw#1
+// let fuc = 10;
+
+// if(fuc => 6){
+//    console.log('grat ig..');
+// }
+
+// let numbah = 23;
+
+// if(numbah == 23){
+//     window.prompt('u passed enter ur name/nickname now!:D');
+// }
+
+// let age = 12;
+
+// if(age <= 11){
+//     console.log('wOaH you must be smarter then meeeh');
+// }
+
+// let p = 0;
+
+// if(p => 1){
+//     console.log('..next now..');
+// }
+
+// let riad = 100;
+
+// if(99 <= riad){
+//     window.prompt('OooOh ur amazing.. so now.. how much is - 11 - 1?');
+// }
+
+// let uh = 98;
+
+// if(uh => 3){
+//    console.log('hm.. oke ig.');
+// }
+
+// let w = 13;
+
+// if(w == 1){
+//     console.log('you... DUMBASS CHILD>:( wrongg..')
+// }
+
+// let idgaf = 90;
+
+// if(idgaf <= 100){
+//     console.log('...');
+// }
+
+// let u = 123;
+
+// if(u == 123){
+//     console.log('*no response.*');
+// }
+
+// let lvltengyaNOTfinishingdas = 29;
+
+// if(11 <= lvltengyaNOTfinishingdas){
+//     console.log('oki.. i guess ur improving a bit..')
+// }
+
+// let fu = 8;
+
+// if(fu <= 4){
+//    console.log('wwwaoh');
+// }
+
+// let num = 3;
+
+// if(numbah == 3){
+//     window.prompt('noice');
+// }
+
+// let ag = 5;
+
+// if(ag <= 4){
+//     console.log('good u smartass..?.');
+// }
+
+// let O = 4;
+
+// if(O => 3){
+//     console.log('gettin happier ig:3');
+// }
+
+// let oasd = 78;
+
+// if(99 <= oasd){
+//     console.log('fuk yeah:)')
+// }
+
+// let Uu = 79;
+
+// if(Uu => 66){
+//    console.log(':D');
+// }
+
+// let bak = 14;
+
+// if(bak == 23){
+//     console.log('you... DUMBASS CHILD>:( wrongg..')
+// }
+
+// let idc = 198;
+
+// if(idgaf <= 100){
+//     console.log('.hm.. k..');
+// }
+
+// let iV = 11;
+
+// if(iV <= 15){
+//     console.log('*no response.*');
+// }
+
+// let zeichentrickserie = 55;
+
+// if(11 <= zeichentrickserie){
+//     console.log('oki.. i guess ur improving a bit..')
+// }
+
+// hw#2
+// let userOnline = true;
+
+// console.log(`user is online : ${userOnline}`)
+
+// let orderServed = true;
+
+// console.log(`our order is served : ${orderServed}`)
+
+// let nickName = true;
+
+// console.log(`my nickname is real : ${nickName}`)
+
+// let carForsale = true;
+
+// console.log(`the cars for sale : ${carForsale}`)
+
+// let grumpyShrek = true;
+
+// console.log(`HAY! shrek IS grumpy! : ${grumpyShrek}`)
+
+// let idunno = true;
+
+// console.log(`idc : ${idunno}`)
+
+// let userlikesPancakes = true;
+
+// console.log(`he lOvEs moi pancakez! : ${userlikesPancakes}`)
+
+// let elsaAintreal = true;
+
+// console.log(`elsa aint real kid.. : ${elsaAintreal}`)
+
+// let santaAintreal = true;
+
+// console.log(`unfortanetly.. santas not real kids:( : ${santaAintreal}`)
+
+// let yummymeal = true;
+
+// console.log(`moms soups r yumm:3 : ${yummymeal}`)
+
+// hw#3
+// let userOnline = false;
+
+// console.log(`user is online : ${userOnline}`)
+
+// let orderServed = false;
+
+// console.log(`our order is served : ${orderServed}`)
+
+// let nickName = false;
+
+// console.log(`my nickname is real : ${nickName}`)
+
+// let carForsale = false;
+
+// console.log(`the cars for sale : ${carForsale}`)
+
+// let grumpyShrek = false;
+
+// console.log(`HAY! shrek IS grumpy! : ${grumpyShrek}`)
+
+// let idunno = false;
+
+// console.log(`idc : ${idunno}`)
+
+// let userlikesPancakes = false;
+
+// console.log(`he lOvEs moi pancakez! : ${userlikesPancakes}`)
+
+// let elsaAintreal = false;
+
+// console.log(`elsa aint real kid.. : ${elsaAintreal}`)
+
+// let santaAintreal = false;
+
+// console.log(`unfortanetly.. santas not real kids:( : ${santaAintreal}`)
+
+// let yummymeal = false;
+
+// console.log(`moms soups r yumm:3 : ${yummymeal}`)
