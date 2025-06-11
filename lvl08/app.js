cabff99e1cd3080140dcaa2f74da37c8ac80c0e6
MiKo//makHo <146019882+Makharadzeelene@users.noreply.github.com>
2025-06-11 21:57:22 +0400

    Add files via upload

---
+++ lvl08/app.js
@@ -0,0 +1,12 @@
+//hw#1
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(let o = 10; o => 0; ){
+//     console.log(o)
+// }
+
+for(let u = 0; u <= 100; u++){
+    console.log(u)
+}
